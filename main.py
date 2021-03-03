2e6174986ce9e78897f407be0cba67107b4febd1
gleb89 <37249444+gleb89@users.noreply.github.com>
2021-03-03 17:48:24 +0300

    fgfg

--- main.py
+++ main.py
@@ -6,13 +6,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-
-parameters = {
-  'start':'1',
-  'limit':'5000',
-  'convert':'USD'
-}
 headers = {
   'Accepts': 'application/json',
   'X-CMC_PRO_API_KEY': '115640cb-4181-4cdb-b65c-f9291338de8f',
@@ -30,12 +23,14 @@ async def get_price(id):
 
     price_usd = data['data'][id]['quote']['USD']['price']
     price_rub = price_usd * 76
-    return name_crypto, price_rub
+    staff = price_rub/ 100 * 10
+    price = price_rub + staff
+    return name_crypto, price
 
   except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
 
-  
+
 @app.get('/')
 async def get_crypto_price(id:str):
   name, price = await get_price(id)
@@ -43,4 +38,6 @@ async def get_crypto_price(id:str):
     'coin':name,
     'price':price
     }
-  
+
+# 1027 - ethereum
+# 1 - bitcoin
